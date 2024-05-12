0aaa502630b8136da38efdb6559f31d44eb07e76
toni <toni.musta@valossa.com>
2024-05-12 20:28:09 +0200

    multiprocessing dataset conversion

--- src/dataset.py
+++ src/dataset.py
@@ -3,38 +3,52 @@ import torch
 from util import read_tenhou_game, construct_words
 import glob, os
 import json
+from multiprocessing.pool import Pool
 
-class MahjongDataset(torch.utils.data.Dataset):
 
-    def __init__(self,path, ratio=1, cache_dir=None):
-        if os.path.isdir(path):
-            self.files = glob.glob(path + "/*")[::ratio]
-        else:
-            self.files = [path]
-        self.words = construct_words()[0] + (["NONE"])
-        self.word_count = len(self.words)
-        self.games = []
-        len_files = len(self.files)
-        for n, f in enumerate(self.files):
+def convert_file(n,f, len_files, cache_dir):
             print(f, n, "/", len_files)
             json_filename = f
             if not f.endswith(".json"):
                 json_filename = cache_dir + "/" + f.split("/")[-1] + ".json"
             if os.path.exists(json_filename):
-                
-                with open(json_filename) as f:
-                    game = json.load(f)
+                pass
+                #with open(json_filename) as f:
+                #    game = json.load(f)
             else:
                 try:
                     game = read_tenhou_game(f)
-                except IndexError:
-                    print("Failure")
-                    continue
-                with open(cache_dir + "/" + json_filename, "w") as f:
+                except :
+                    print("Failure", f)
+                    return
+                with open( json_filename, "w") as f:
                     json.dump(game, f)
-            for i in game:
-                for j in i:
-                    self.games.append(j)
+
+
+class MahjongDataset(torch.utils.data.Dataset):
+
+    def __init__(self,path,  cache_dir, ratio=1):
+        if os.path.isdir(path):
+            self.files = glob.glob(path + "/*")[::ratio]
+        else:
+            self.files = [path]
+        self.cache_dir = cache_dir
+        self.words = construct_words()[0] + (["NONE"])
+        self.word_count = len(self.words)
+        self.games = []
+        self.len_files = len(self.files)
+        print(self.len_files)
+        self.convert_dataset()
+        #for filename in glob.glob(cache_dir + "/*"):
+        for filename in self.files:
+            if os.path.exists(filename):
+                
+                with open(filename) as f:
+                    game = json.load(f)
+                for i in game:
+                    for j in i:
+                        self.games.append(j)
+        print(len(self.games))
         #self.cut_off_points = [[n for n, y in enumerate(x) if len(y)==2 and n > 18 ] for x in self.games]
         self.cut_off_points = [[n for n, y in enumerate(x) if "P0" in y and n > 18 ] for x in self.games]
         self.games = [[self.words.index(y) for y in x] for x in self.games]
@@ -69,6 +83,17 @@ class MahjongDataset(torch.utils.data.Dataset):
     def print_game(self, game):
         return [self.words[x] for x in game]
 
+    
+    def convert_dataset(self):
+        #for n, f in enumerate(self.files):
+        
+            #for i in game:
+            #    for j in i:
+            #        self.games.append(j)
+        self_files = [(x,y,self.len_files, self.cache_dir) for x,y in enumerate(self.files) if not os.path.exists(self.cache_dir + "/" + y.split("/")[-1] + ".json")]
+        with Pool() as p:
+            p.starmap(convert_file, self_files)
+    
 if __name__=="__main__":
 
     md = MahjongDataset("tmp", cache_dir="tmp")
